0065bc0c275ad653968348020c3e028e7d7d7227
BoburF <changeitgo3@gmail.com>
2025-04-03 19:56:30 +0500

    [*] flags are set for argument of what to read

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	common_util "github.com/BoburF/advent/common-util"
 )
 
 func main() {
-	fmt.Println("Hello world!")
-	data, err := common_util.ReadFile(2015, 1)
+  year := flag.Int("y", 2015, "year of advent")
+  day := flag.Int("d", 1, "day of advent year")
+
+  flag.Parse()
+
+	data, err := common_util.ReadFile(*year, *day)
 	if err != nil {
 		fmt.Println("Error reading input file:", err)
 		return
