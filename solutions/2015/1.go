a3d529c29a1eb5fd17338ab83dadf9a138e9975c
BoburF <changeitgo3@gmail.com>
2025-04-04 10:31:38 +0500

    [*] day 1 of 2015 is completed

--- solutions/2015/1.go
+++ solutions/2015/1.go
@@ -2,6 +2,33 @@ package solutions_2015
 
 import "fmt"
 
+// func Solution20151(data []byte) {
+// 	count := 0
+// 	for _, ch := range data {
+// 		switch ch {
+// 		case '(':
+// 			count++
+// 		case ')':
+// 			count--
+// 		}
+// 	}
+//
+// 	fmt.Println("Result:", count)
+// }
+
 func Solution20151(data []byte) {
-	fmt.Println("Hello first solution!")
+	count := 0
+	for idx, ch := range data {
+		switch ch {
+		case '(':
+			count++
+		case ')':
+			count--
+		}
+
+		if count == -1 {
+			fmt.Println("Result:", idx+1)
+			break
+		}
+	}
 }
